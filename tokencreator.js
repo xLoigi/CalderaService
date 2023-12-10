063f70579bc0bca1327472cb3f9f5592613a3608
xLoigi <79259439+xLoigi@users.noreply.github.com>
2023-12-10 21:11:10 +0100

    v1

---
+++ tokencreator.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
+
+const guids = require("./guids.json")
+
+const config = JSON.parse(fs.readFileSync("./config.json").toString());
+
+var CalderaGenerated = 0;
+
+function createCaldera(accountId) {
+    let CalderaToken = jwt.sign({
+        "account_id": accountId,
+        "generated": CalderaGenerated =+ 1,
+        "calderaGuid": guids[config.version],
+        "acProvider": "none",
+        "notes": "",
+        "fallback": false
+    }, global.JWT_SECRET );
+
+    return CalderaToken
+}
+
+module.exports = {
+
+    createCaldera
+}
